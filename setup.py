f1260ec68dd5578fd93e1f9d6d14e49dc5bebdc1
Owen Campbell <owen.campbell@tanti.org.uk>
2016-09-03 18:52:42 +0100

    Add licence

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='xontrib-thefuck',
-    version='0.1',
+    version='0.2',
     author='Owen Campbell',
     description="Xonsh extension for thefuck",
     packages=['xontrib'],
