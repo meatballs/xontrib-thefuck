8a929963884888270a5774afa8b1e1060a20d50e
Owen Campbell <owen.campbell@tanti.org.uk>
2016-09-04 14:54:36 +0100

    Use __getitem__

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='xontrib-thefuck',
-    version='0.3',
+    version='0.3.1',
     author='Owen Campbell',
     description="Xonsh extension for thefuck",
     packages=['xontrib'],
--- xontrib/thefuck.py
+++ xontrib/thefuck.py
@@ -2,7 +2,7 @@ from builtins import aliases, __xonsh_history__, evalx
 
 
 def _new_command(args, stdin=None):
-    last_command = __xonsh_history__[-1]
+    last_command = __xonsh_history__.__getitem__(-1)
     evalx('thefuck {}'.format(last_command))
 
 aliases['fuck'] = _new_command
