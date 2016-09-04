bb963e046db66f66b141d8c242a13d8c42258fb8
Owen Campbell <owen.campbell@tanti.org.uk>
2016-09-04 14:44:21 +0100

    Amend history object for xonsh 0.4.6

--- xontrib/thefuck.py
+++ xontrib/thefuck.py
@@ -2,7 +2,7 @@ from builtins import aliases, __xonsh_history__, evalx
 
 
 def _new_command(args, stdin=None):
-    last_command = __xonsh_history__.show("session")[-1][0]
+    last_command = __xonsh_history__[-1]
     evalx('thefuck {}'.format(last_command))
 
 aliases['fuck'] = _new_command
