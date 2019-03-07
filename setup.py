acb77751eb771940208afa356c536f6aab572131
Owen Campbell <owen.campbell@empiria.co.uk>
2019-03-07 10:39:29 +0000

    Bump version to 0.3.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='xontrib-thefuck',
-    version='0.3.1',
+    version='0.3.2',
     author='Owen Campbell',
     description="Xonsh extension for thefuck",
     packages=['xontrib'],
