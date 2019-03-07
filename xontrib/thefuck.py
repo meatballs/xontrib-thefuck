25c1c791cdf3d748f2188bb1b79a0da4ef5d827f
Owen Campbell <owen.campbell@empiria.co.uk>
2019-03-07 11:18:46 +0000

    Remove whitespace from last_command

--- xontrib/thefuck.py
+++ xontrib/thefuck.py
@@ -2,7 +2,8 @@ from builtins import aliases, __xonsh__, evalx
 
 
 def _new_command(args, stdin=None):
-    last_command = __xonsh__.history[-1].cmd
+    last_command = __xonsh__.history[-1].cmd.strip()
+
     execx('thefuck {}'.format(last_command))
 
 aliases['fuck'] = _new_command
