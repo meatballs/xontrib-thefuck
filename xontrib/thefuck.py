9f45a571689475bca214da241b3a04ea43401151
Owen Campbell <owen.campbell@empiria.co.uk>
2019-03-07 10:21:10 +0000

    Correct last_command evaluation

--- xontrib/thefuck.py
+++ xontrib/thefuck.py
@@ -2,7 +2,7 @@ from builtins import aliases, __xonsh_history__, evalx
 
 
 def _new_command(args, stdin=None):
-    last_command = __xonsh__.history.__getitem__(-1)
+    last_command = __xonsh__.history[-1].cmd
     evalx('thefuck {}'.format(last_command))
 
 aliases['fuck'] = _new_command
