7ae737c90993b6f4de85a03675927203f7309b3d
Owen Campbell <owen.campbell@empiria.co.uk>
2019-03-07 10:38:39 +0000

    Strip CR from last_command

--- xontrib/thefuck.py
+++ xontrib/thefuck.py
@@ -2,7 +2,7 @@ from builtins import aliases, __xonsh__.history, evalx
 
 
 def _new_command(args, stdin=None):
-    last_command = __xonsh__.history[-1].cmd
+    last_command = __xonsh__.history[-1].cmd.strip()
     evalx('thefuck {}'.format(last_command))
 
 aliases['fuck'] = _new_command
